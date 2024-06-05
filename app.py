b00d3db7d5a541413c423a2f092890e10cf5db4d
garfield <otienot75@gmail.com>
2024-06-05 11:08:06 +0300

    fixing wordings, again

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def web_hook():
                     
                     elif client_input == "/about":
                         image_list = ['./static/bot_images/landing_bot_image.jpeg']
-                        message_test = f"{user_name}\n\n, I am spendvest bot, I can help you with your micro-savings plan!To access services press /refresh"
+                        message_test = f"{user_name}\n\n, I am spendvest bot, I can help you with your micro-savings plan!\nTo access services press /refresh"
                         return test_message_with_image(message_test,image_list)
                     
 
