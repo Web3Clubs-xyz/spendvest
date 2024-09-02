1c81c7a3da619cbff64e69f5c8a6d11aa6748d84
ong3ri <ong3ri@ong3r1.com>
2024-09-02 17:41:28 +0300

    configuring uvicorn port for cpanel

--- run_server.py
+++ run_server.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=80,
+        port=8000,
         reload=True,  # Enable auto-reload
         workers=4,  # Number of worker processes
         log_level="info",
